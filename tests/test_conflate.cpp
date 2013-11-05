14a0185dcccb04c78fc8cfec4d79bfc84c51fa8f
Mike Wake <macwake@gmail.com>
2013-11-05 19:02:53 +1100

    Some sctp tests based on existing tcp tests.
    - test_conflate
    - test_immediate_sctp

--- tests/test_conflate.cpp
+++ tests/test_conflate.cpp
@@ -21,52 +21,61 @@
 
 int main (int argc, char *argv [])
 {
-    const char *bind_to = "tcp://127.0.0.1:5555";
-
+    const char* addresses[] = {"tcp://127.0.0.1:5555", "sctp://127.0.0.1:6555"};
+    const char *bind_to;
     int rc;
+    void* ctx;
+    void* s_in;
+    void* s_out;
+    int conflate;
+    int message_count;
+    int payload_recved;
 
-    void* ctx = zmq_init (1);
+    ctx = zmq_init (1);
     assert (ctx);
 
-    void* s_in = zmq_socket (ctx, ZMQ_PULL);
-    assert (s_in);
+    for(int i=0; i < 2; ++i)
+    {
+        bind_to = addresses[i];
 
-    int conflate = 1;
-    rc = zmq_setsockopt (s_in, ZMQ_CONFLATE, &conflate, sizeof(conflate));
-    assert (rc == 0);
+        s_in = zmq_socket (ctx, ZMQ_PULL);
+        assert (s_in);
 
-    rc = zmq_bind (s_in, bind_to);
-    assert (rc == 0);
+        rc = zmq_setsockopt (s_in, ZMQ_CONFLATE, &conflate, sizeof(conflate));
+        assert (rc == 0);
 
-    void* s_out = zmq_socket (ctx, ZMQ_PUSH);
-    assert (s_out);
+        rc = zmq_bind (s_in, bind_to);
+        assert (rc == 0);
 
-    rc = zmq_connect (s_out, bind_to);
-    assert (rc == 0);
+        s_out = zmq_socket (ctx, ZMQ_PUSH);
+        assert (s_out);
 
-    int message_count = 20;
+        rc = zmq_connect (s_out, bind_to);
+        assert (rc == 0);
 
-    for (int j = 0; j < message_count; ++j) {
-        rc = zmq_send(s_out, (void*)&j, sizeof(int), 0);
-        if (rc < 0) {
-            printf ("error in zmq_sendmsg: %s\n", zmq_strerror (errno));
-            return -1;
+        message_count = 20;
+
+        for (int j = 0; j < message_count; ++j) {
+            rc = zmq_send(s_out, (void*)&j, sizeof(int), 0);
+            if (rc < 0) {
+                printf ("error in zmq_sendmsg: %s\n", zmq_strerror (errno));
+                return -1;
+            }
         }
+
+        zmq_sleep (1);
+
+        payload_recved = 0;
+        rc = zmq_recv(s_in, (void*)&payload_recved, sizeof(int), 0);
+        assert (rc > 0);
+        assert (payload_recved == message_count - 1);
+
+        rc = zmq_close (s_in);
+        assert (rc == 0);
+
+        rc = zmq_close (s_out);
+        assert (rc == 0);
     }
-
-    zmq_sleep (1);
-
-    int payload_recved = 0;
-    rc = zmq_recv(s_in, (void*)&payload_recved, sizeof(int), 0);
-    assert (rc > 0);
-    assert (payload_recved == message_count - 1);
-
-
-    rc = zmq_close (s_in);
-    assert (rc == 0);
-
-    rc = zmq_close (s_out);
-    assert (rc == 0);
 
     rc = zmq_term (ctx);
     assert (rc == 0);
